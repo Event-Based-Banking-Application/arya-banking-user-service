0e7c778007f03934f6134080c37ebe582916cbd2
kartik1502 <kartikkulkarni1411@gmail.com>
2025-08-10 10:55:27 +0530

    feat: update server port and enable eureka client.

--- src/main/java/org/arya/banking/user/AryaBankingUserServiceApplication.java
+++ src/main/java/org/arya/banking/user/AryaBankingUserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
@@ -15,6 +16,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 })
 @ComponentScan(basePackages = {"org.arya.banking.user", "org.arya.banking.common"})
 @EnableMongoAuditing
+@EnableDiscoveryClient
 public class AryaBankingUserServiceApplication {
 
     public static void main(String[] args) {
