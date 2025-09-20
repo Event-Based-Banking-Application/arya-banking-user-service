540334b56c4aba7a76d70b639403239b0bae7732
Karthik Kulkarni <64131481+kartik1502@users.noreply.github.com>
2025-09-20 23:15:20 +0530

    feat: emit event on user registration. (#98)
    Emit the user registration event, connect to kafka on start-up.
    
    Refs: #94

--- src/main/java/org/arya/banking/user/config/UserServiceMongoConfig.java
+++ src/main/java/org/arya/banking/user/config/UserServiceMongoConfig.java
@@ -1,9 +1,18 @@
 package org.arya.banking.user.config;
 
+import org.arya.banking.common.avro.UserCreateEvent;
 import org.arya.banking.common.config.MongoConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Import(MongoConfig.class)
 public class UserServiceMongoConfig {
+
+    @Bean
+    public KafkaTemplate<String, UserCreateEvent> userCreateTemplate(ProducerFactory<String, UserCreateEvent> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
 
 }
---
+++ src/main/java/org/arya/banking/user/config/kafka/UserCreateProducer.java
@@ -0,0 +1,21 @@
+package org.arya.banking.user.config.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.arya.banking.common.avro.UserCreateEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UserCreateProducer {
+
+    private final KafkaTemplate<String, UserCreateEvent> userCreateEventTemplate;
+
+    public void sendUserCreateEvent(UserCreateEvent userCreateEvent) {
+        userCreateEventTemplate.send("user-create-event", userCreateEvent.getUserId().toString(), userCreateEvent);
+        log.info("User create event sent for: {}", userCreateEvent.getUserId());
+    }
+
+}
