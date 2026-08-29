41a85c92144c6bb6e52458dc8a1553aadfd4b0bf
kartik1502 <kartikkulkarni1411@gmail.com>
2026-08-30 01:37:45 +0530

    feat: enable AspectJ auto proxy support and update arya-banking-common version to 1.2.5

--- src/main/java/org/arya/banking/user/AryaBankingUserServiceApplication.java
+++ src/main/java/org/arya/banking/user/AryaBankingUserServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Slf4j
@@ -21,6 +22,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @ComponentScan(basePackages = {"org.arya.banking.user", "org.arya.banking.common"})
 @EnableMongoAuditing
 @EnableDiscoveryClient
+@EnableAspectJAutoProxy
 @EnableFeignClients(defaultConfiguration = OAuth2FeignConfig.class)
 public class AryaBankingUserServiceApplication {
 
